3106c2014dc648d29deca4a7a60879adccbd5343
Hyunmin Jung <jung@hyunmin.dev>
2021-03-20 12:45:29 +0900

    Add item class

---
+++ item.cpp
@@ -0,0 +1,6 @@
+//
+// Created by 정현민 on 2021/03/20.
+//
+
+#include "item.h"
+Item::Item() {}
---
+++ item.h
@@ -0,0 +1,15 @@
+//
+// Created by 정현민 on 2021/03/20.
+//
+
+#ifndef PROJECT__ITEM_H_
+#define PROJECT__ITEM_H_
+
+class Item {
+ public:
+  Item();
+ private:
+
+};
+
+#endif //PROJECT__ITEM_H_
