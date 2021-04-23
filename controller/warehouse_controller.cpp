53d1f803cc6ce8ffe15dd13fe8e9583c48d062b7
Dongmin Kim <ehdals0211@naver.com>
2021-04-23 13:35:38 +0900

    build warehouse controller basic

--- controller/warehouse_controller.cpp
+++ controller/warehouse_controller.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "warehouse_controller.h"
+
+WarehouseController::WarehouseController() {
+  this->ReadWarehouse();
+}
+
+WarehouseController::~WarehouseController() = default;
+
+void WarehouseController::ReadWarehouse() {
+  std::ifstream read_warehouse_file;
+  read_warehouse_file.open("warehouse.txt", std::ios::in); //file name
+  if (read_warehouse_file.is_open()) {
+    while (!read_warehouse_file.eof()) {
+      std::string line = "\n";
+      getline(read_warehouse_file, line, '\n');
+      if (line.empty()) continue;
+      std::vector<std::string> user_information = StringHandler::SplitString(line, '\t');
+
+    }
+  } else {
+    std::ofstream new_file("auth.txt");
+    new_file.close();
+  }
+  read_warehouse_file.close();
+}
--- controller/warehouse_controller.h
+++ controller/warehouse_controller.h
@@ -5,8 +5,29 @@
 #ifndef PROJECT_CONTROLLER_WAREHOUSE_CONTROLLER_H_
 #define PROJECT_CONTROLLER_WAREHOUSE_CONTROLLER_H_
 
-class WarehouseController {
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 
+#include "../handler/output_handler.h"
+#include "../handler/string_handler.h"
+#include "../model/item_model.h"
+#include "../model/warehouse_model.h"
+
+struct WarehouseState{
+  WarehouseModel warehouse;
+  std::vector<std::pair<ItemModel,int>> items_state;
+};
+
+class WarehouseController {
+ public:
+  explicit WarehouseController();
+  ~WarehouseController();
+  void ReadWarehouse();
+  void Receive();
+ private:
+  std::vector<WarehouseState> warehouse_state_;
 };
 
 #endif //PROJECT_CONTROLLER_WAREHOUSE_CONTROLLER_H_
